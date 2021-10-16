6933b2b8fc2d4a161bbc3eb357f2a60909499450
kizenkov <kizenkov@tut.by>
2021-10-17 02:25:31 +0300

    refactoring

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,34 +1,26 @@
 import {useState} from 'react'
 
 function Header({setLatLon}) {
-    const [location, setLocation] = useState('')
     const [latLonName, setLatLonName] = useState(['Минск', 'BY'])
     const getWeather = (e) => {
-        if (e.preventDefault) {
-            e.preventDefault()
-        } else {
-            e.returnValue = false
-        }
-        fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=5&appid=e66c7ec7a8be8c638d481b97895d23ee`)
+        e.preventDefault()
+        fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${e.currentTarget[0].value}&limit=5&appid=e66c7ec7a8be8c638d481b97895d23ee`)
             .then(response => response.json())
             .then(response => {
                 setLatLonName([response[0].local_names.ru, response[0].country])
                 setLatLon([response[0].lat, response[0].lon])
             })
             .catch(err => {
-                // console.log(err)
-                alert(err)
+                console.log(err)
             })
-        // setLocation('')
+        e.currentTarget[0].value = ''
     }
 
     return <div className='header'>
         <form onSubmit={getWeather} className='inline'>
             <input type='text'
                    required
-                   value={location}
                    name='locationfield'
-                   onChange={(e) => setLocation(e.target.value)}
                    placeholder='Поиск местоположения'/>
         </form>
         <h4 className='inline'>{latLonName[0]}, {latLonName[1]}</h4>
