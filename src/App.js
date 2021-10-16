cacd8156025826a8724132ab59e100f012de7b68
kizenkov <kizenkov@tut.by>
2021-10-17 00:01:44 +0300

    footer and margin

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import {useEffect, useState} from "react"
 import vector from './vector.png'
+import Footer from "./Footer";
 
 
 function App() {
@@ -39,7 +40,7 @@ function App() {
                         </div>
                     </div>
 
-                    <div className='inline'>
+                    <div className='inline information'>
                         <div className='inline'>
                             <div>
                                 ВЕТЕР
@@ -94,30 +95,6 @@ function App() {
                             </div>
                         </div>
                     </div>
-
-
-                    {/*<div className="inline">*/}
-                    {/*    <div>Утром {Math.trunc(+data.daily[i].temp.morn - 273.15)}°</div>*/}
-                    {/*    <div>Днём {Math.trunc(+data.daily[i].temp.day - 273.15)}°</div>*/}
-                    {/*    <div>Вечером {Math.trunc(+data.daily[i].temp.eve - 273.15)}°</div>*/}
-                    {/*    <div>Ночью {Math.trunc(+data.daily[i].temp.night - 273.15)}°</div>*/}
-                    {/*</div>*/}
-                    {/*<div className="inline">*/}
-                    {/*    <div className='wind'>*/}
-                    {/*        Скорость ветра {Math.round(+data.daily[i].wind_speed * 10) / 10} м/c*/}
-                    {/*        <img src={vector} alt="" style={{transform: `rotate(${+data.daily[i].wind_deg}deg)`}}/>*/}
-                    {/*    </div>*/}
-                    {/*    <div>Порывы ветра до {Math.round(+data.daily[i].wind_gust * 10) / 10} м/с</div>*/}
-                    {/*</div>*/}
-                    {/*<div className="inline">*/}
-                    {/*    <div>Облачность {data.daily[i].clouds}%</div>*/}
-                    {/*    <div>Давление {Math.round(+data.daily[i].pressure / 1.33)} мм.рт.ст.</div>*/}
-                    {/*    <div>Влажность {data.daily[i].humidity}%</div>*/}
-                    {/*</div>*/}
-                    {/*<div className="inline">*/}
-                    {/*    <div>Восход {new Date(+data.daily[i].sunrise * 1000).toLocaleTimeString()}</div>*/}
-                    {/*    <div>Закат {new Date(+data.daily[i].sunset * 1000).toLocaleTimeString()}</div>*/}
-                    {/*</div>*/}
                 </div>
             </div>)
         }
@@ -153,104 +130,107 @@ function App() {
     }, [latLonName])
 
 
-    return (
-        <div className='app'>
-            <form onSubmit={getWeather} className='inline'>
-                <input type='text'
-                       value={location}
-                       onChange={(e) => setLocation(e.target.value)}
-                       placeholder='Поиск местоположения'/>
-            </form>
-            <h4 className='inline'>{latLonName[2]}, {latLonName[3]}</h4>
-            {data && <div className="current">
+    return (<div>
+            <div className='app'>
+                <form onSubmit={getWeather} className='inline'>
+                    <input type='text'
+                           value={location}
+                           onChange={(e) => setLocation(e.target.value)}
+                           placeholder='Поиск местоположения'/>
+                </form>
+                <h4 className='inline'>{latLonName[2]}, {latLonName[3]}</h4>
+                {data && <div className="current">
+                    <h5>
+                        ТЕКУЩАЯ ПОГОДА
+                    </h5>
+                    <div>
+                        <div>
+                            {time.toLocaleTimeString().slice(0, -3)}
+                        </div>
+                        <div>
+                            {`${day} ${month} ${year}`}
+                        </div>
+                    </div>
+                    <div className='current-weather'>
+                        <div>
+                            <div className='currentTemp inline'>
+                                <img src={`http://openweathermap.org/img/wn/${data.current.weather[0].icon}@2x.png`}
+                                     alt="icon"/>
+                                <span>{Math.trunc(+data.current.temp - 273.15)}°</span>
+                            </div>
+                            <div className='inline'>
+                                <div className='description'>
+                                    {data.current.weather[0].description[0].toUpperCase() + data.current.weather[0].description.slice(1)}
+                                </div>
+                                <div>Ощущается как {Math.trunc(+data.current.feels_like - 273.15)}°</div>
+                            </div>
+                        </div>
+
+                        <div>
+                            <div className='inline'>
+                                <div>
+                                    ВЕТЕР
+                                </div>
+                                <div className='wind description'>
+                                    {Math.round(+data.current.wind_speed * 10) / 10} м/c
+                                    <img src={vector} alt=""
+                                         style={{transform: `rotate(${+data.current.wind_deg}deg)`}}/>
+                                </div>
+                            </div>
+                            <div className='inline'>
+                                <div>
+                                    ПОРЫВЫ
+                                </div>
+                                <div className='wind description'>
+                                    до {Math.round(+data.current.wind_gust * 10) / 10} м/с
+                                </div>
+                            </div>
+                            <div className='inline'>
+                                <div>
+                                    ВЛАЖНОСТЬ
+                                </div>
+                                <div className='description'>
+                                    {data.current.humidity}%
+                                </div>
+                            </div>
+                            <div className='inline'>
+                                <div>
+                                    ВИДИМОСТЬ
+                                </div>
+                                <div className='description'>
+                                    {data.current.visibility / 1000} км
+                                </div>
+                            </div>
+                            <div className='inline'>
+                                <div>
+                                    ОБЛАЧНОСТЬ
+                                </div>
+                                <div className='description'>
+                                    {data.current.clouds}%
+                                </div>
+                            </div>
+                            <div className='inline'>
+                                <div>
+                                    ДАВЛЕНИЕ
+                                </div>
+                                <div className='description'>
+                                    {Math.round(+data.current.pressure / 1.33)} мм
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>}
                 <h5>
-                    ТЕКУЩАЯ ПОГОДА
+                    ПРОГНОЗ НА 7 ДНЕЙ
                 </h5>
-                <div>
-                    <div>
-                        {time.toLocaleTimeString().slice(0, -3)}
-                    </div>
-                    <div>
-                        {`${day} ${month} ${year}`}
-                    </div>
+                <div className="overflow">
+                    {dailyList}
                 </div>
-                <div className='current-weather'>
-                    <div>
-                        <div className='currentTemp inline'>
-                            <img src={`http://openweathermap.org/img/wn/${data.current.weather[0].icon}@2x.png`}
-                                 alt="icon"/>
-                            <span>{Math.trunc(+data.current.temp - 273.15)}°</span>
-                        </div>
-                        <div className='inline'>
-                            <div className='description'>
-                                {data.current.weather[0].description[0].toUpperCase() + data.current.weather[0].description.slice(1)}
-                            </div>
-                            <div>Ощущается как {Math.trunc(+data.current.feels_like - 273.15)}°</div>
-                        </div>
-                    </div>
+                <div className='today'>
 
-                    <div>
-                        <div className='inline'>
-                            <div>
-                                ВЕТЕР
-                            </div>
-                            <div className='wind description'>
-                                {Math.round(+data.current.wind_speed * 10) / 10} м/c
-                                <img src={vector} alt="" style={{transform: `rotate(${+data.current.wind_deg}deg)`}}/>
-                            </div>
-                        </div>
-                        <div className='inline'>
-                            <div>
-                                ПОРЫВЫ
-                            </div>
-                            <div className='wind description'>
-                                до {Math.round(+data.current.wind_gust * 10) / 10} м/с
-                            </div>
-                        </div>
-                        <div className='inline'>
-                            <div>
-                                ВЛАЖНОСТЬ
-                            </div>
-                            <div className='description'>
-                                {data.current.humidity}%
-                            </div>
-                        </div>
-                        <div className='inline'>
-                            <div>
-                                ВИДИМОСТЬ
-                            </div>
-                            <div className='description'>
-                                {data.current.visibility / 1000} км
-                            </div>
-                        </div>
-                        <div className='inline'>
-                            <div>
-                                ОБЛАЧНОСТЬ
-                            </div>
-                            <div className='description'>
-                                {data.current.clouds}%
-                            </div>
-                        </div>
-                        <div className='inline'>
-                            <div>
-                                ДАВЛЕНИЕ
-                            </div>
-                            <div className='description'>
-                                {Math.round(+data.current.pressure / 1.33)} мм
-                            </div>
-                        </div>
-                    </div>
                 </div>
-            </div>}
-            <h5>
-                ПРОГНОЗ НА 7 ДНЕЙ
-            </h5>
-            <div className="overflow">
-                {dailyList}
             </div>
-            <div className='today'>
-
-            </div>
+            <Footer/>
         </div>
     )
 }
---
+++ src/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer() {
+    return <div className='footer'>
+        <div>Weather in Minsk and not only</div>
+        <div>2021</div>
+    </div>
+}
+
+export default Footer
