0a33e7bf6110f61520d3441d254b380ff25c01d7
kizenkov <kizenkov@tut.by>
2021-10-17 12:12:06 +0300

    refactoring

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,5 +1,4 @@
 import {useState} from 'react'
-import * as axios from 'axios'
 
 function Header({setLatLon}) {
 
@@ -7,30 +6,26 @@ function Header({setLatLon}) {
     const [location, setLocation] = useState('')
     const getWeather = (e) => {
         e.preventDefault()
-        let url = `https://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=5&appid=e66c7ec7a8be8c638d481b97895d23ee`
-        alert(url)
-        axios.get(url)
+        fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=5&appid=e66c7ec7a8be8c638d481b97895d23ee`)
             .then(response => {
                 let data = response.data
                 setLatLonName([data[0].local_names.ru, data[0].country])
                 setLatLon([data[0].lat, data[0].lon])
             })
             .catch(err => {
-                alert(err)
-                // console.log(err)
+                console.log(err)
             })
         setLocation('')
     }
 
     return <div className='header'>
-        {/*<form onSubmit={getWeather} className='inline'>*/}
+        <form onSubmit={getWeather} className='inline'>
             <input type='text'
                    required
                    value={location}
                    onChange={(e) => setLocation(e.target.value)}
                    placeholder='Поиск местоположения'/>
-            <button onClick={getWeather}>send</button>
-        {/*</form>*/}
+        </form>
         <h4 className='inline'>{latLonName[0]}, {latLonName[1]}</h4>
     </div>
 }
